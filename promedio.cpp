03fd1dd7e559ca77fdc7e72c40bc21075e9e05c0
BerlyCS <berly.mandamientos@gmail.com>
2024-07-12 11:22:07 -0500

    programa para calcular promedio

--- promedio.cpp
+++ promedio.cpp
@@ -3,24 +3,71 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    float args[12];
+bool no_es_numero(string str) {
+    for (char i: str) {
+        if ( '0' < i || '9' > i || '.' != i ) {
+            return false;
+        } 
+    }
+    return true;
+}
 
-    for (int i=0; i<12; i++) {
-        args[i] = stof(argv[i]);
+bool tratar_argv(int argc, char* argv[]) {
+    if (argc < 13) {
+        cout<<"Argumentos insuficientes\n";
+        return false;
+    }
+    for (int i=1; i<argc; i++) {
+        if (no_es_numero(string(argv[i]))) {
+            cout<<"Argumento no numerico"<<endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+
+    //first
+    float notas[6];
+    float pesos[6];
+    for (int i=0; i<6; i++) {
+        cout<<argv[i+1]<<' '<<argv[i+7]<<endl;
+        notas[i] = stof(argv[i+1]);
+        pesos[i] = stof(argv[i+7]);
     }
 
-    int zero_count = 0;
-    
-    (nc1 > 0) ?  : zero_count++;
-    (ne1 > 0) ?  : zero_count++;
-    (nc2 > 0) ?  : zero_count++;
-    (ne2 > 0) ?  : zero_count++;
-    (nc3 > 0) ?  : zero_count++;
-    (ne3 > 0) ?  : zero_count++;
+    /* float test=0; */
+    /* for (int i=0;i<6;i++) { */
+    /*     test+=pesos[0]; */
+    /* } */
+    /* if (test != float(1.00)) { */
+    /*     cout<<"W: Los pesos no suman 1.00"<<endl; */
+    /* } */
 
-    cout<<"Notas con 0: "<<zero_count<<endl;
+    bool zero_count[6];
 
-    
-    
+    for (int i=0; i<6; i++) {
+        if (notas[i] < 1) {
+            zero_count[i] = true;
+        } else {
+            zero_count[i] = false;
+        }
+    }
+
+    float count=0, nota_pond=0, nota_falta=0, peso_falta=0;
+    for (int i=0;i<6;i++) {
+        if ( !zero_count[i] ) {
+            nota_pond += notas[i]*pesos[i];
+        } else {
+            peso_falta+=pesos[i];
+            count++;
+        }
+    }
+
+    cout<<peso_falta<<endl;
+    cout<<nota_pond<<endl;
+
+    //Usando  
+    cout<<"Tendrias que sacar: "<<((10.5f-nota_pond)/peso_falta)<<" para aprobar"<<endl;
 }
