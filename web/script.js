770dd388caa07f44cfd48a9f1906d421fa9b72fd
BerlyCS <>
2025-08-17 22:25:27 -0500

    Advacements

--- web/script.js
+++ web/script.js
@@ -1,2 +1,21 @@
-let test = fetch('data.json')
-console.log(test)
+function computeGrade() {
+  
+}
+
+function test() {console.log("si")}
+
+async function getData() {
+  try {
+    const response = await fetch("./data.json");
+    if (!response.ok) { throw new Error('Response status ${response.status}') }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
+
+let data = getData();
+
+console.log(data);
